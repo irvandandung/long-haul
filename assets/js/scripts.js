718d26bbf685f91bf293a2b14d1da748020f6ce2
Muhamad Irvan Dandung <muhamad.dandung@somatech.co>
2020-11-10 14:04:42 +0700

    update function for button up

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,6 +13,20 @@ $( document ).ready(function() {
       return Math.round(parseFloat(value));
     });
 
+    var btn = $('#buttonUp');
+
+	$(window).scroll(function() {
+	  if ($(window).scrollTop() > 300) {
+	    btn.addClass('show');
+	  } else {
+	    btn.removeClass('show');
+	  }
+	});
+
+	btn.on('click', function(e) {
+	  e.preventDefault();
+	  $('html, body').animate({scrollTop:0}, '300');
+	});
 });
 
 
